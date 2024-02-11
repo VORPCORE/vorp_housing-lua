4e9c06583f0f8bd8e8ad25e107b00090b7a963d1
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-02-11 10:28:45 +0000

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,11 @@
-
-
-game 'rdr3'
 fx_version 'cerulean'
-
-author 'VORP ASHKAN'
-description 'VORP Housing'
+game 'rdr3'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
+
+author 'VORP @ASHKAN'
+description 'A player Housing script for vorp core framework'
+repository 'https://github.com/VORPCORE/vorp_housing-lua'
 lua54 'yes'
 
 
@@ -15,14 +14,14 @@ shared_scripts {
 	"locale.lua",
 	"languages/*.lua"
 }
-
-client_scripts {
-   'client/client.lua'
-}
-
+client_script 'client/client.lua'
 server_scripts {
    'server/server.lua',
    '@oxmysql/lib/MySQL.lua'
 }
 
 
+version '1.0'
+vorp_checker 'yes'
+vorp_name '^4Resource version Check^3'
+vorp_github 'https://github.com/VORPCORE/vorp_housing-lua'
